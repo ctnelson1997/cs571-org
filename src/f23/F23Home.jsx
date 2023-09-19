285d8fd71be89f5c10ffdb2b1851f46cf9329aa8
Cole Nelson <ctnelson1997@gmail.com>
2023-09-19 08:02:53 -0500

    maint

--- src/f23/F23Home.jsx
+++ src/f23/F23Home.jsx
@@ -1,24 +1,27 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Alert, Col, Container, Row } from 'react-bootstrap';
 import F23About from './components/F23About';
 import F23Schedule from './components/FS23Schedule';
 
 function F23Home() {
   return (
-    <div className="pad">
-      <Container>
-        <Row>
-          <h1>Welcome to CS571 Fall 2023!</h1>
-        </Row>
-        <Row>
-          <Col xs={12} lg={6}>
-            <F23About />
-          </Col>
-          <Col xs={12} lg={6}>
-            <F23Schedule />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <>
+      <Alert variant='warning'><a href="https://www.cs571.org/">cs571.org</a> will be taken down between 9 and 11 pm CT on Wednesday, Sept 20th for planned maintenance. The website and/or APIs may be unresponsive during this time.</Alert>
+      <div className="pad">
+        <Container>
+          <Row>
+            <h1>Welcome to CS571 Fall 2023!</h1>
+          </Row>
+          <Row>
+            <Col xs={12} lg={6}>
+              <F23About />
+            </Col>
+            <Col xs={12} lg={6}>
+              <F23Schedule />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
