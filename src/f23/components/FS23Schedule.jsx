4f58a48cc5dfedbf35d71b78d46e060c53d636f6
Cole Nelson <ctnelson1997@gmail.com>
2023-11-07 17:50:32 -0600

    w10 rm2

--- src/f23/components/FS23Schedule.jsx
+++ src/f23/components/FS23Schedule.jsx
@@ -70,7 +70,7 @@ export default function F23Schedule() {
                 </tr>
                 <tr>
                     <td>Nov 6<br/></td>
-                    <td>React Native 2 <a target="_blank" href="https://github.com/CS571-F23/lectures/blob/main/Week10/Week10-ReactNative2/week10-reactnative2.pdf">(N)</a> & Mobile Design</td>
+                    <td>React Native 2 <a target="_blank" href="https://github.com/CS571-F23/lectures/blob/main/Week10/Week10-ReactNative2/week10-reactnative2.pdf">(N)</a> <a target="_blank" href="https://cdnapisec.kaltura.com/p/1660902/sp/166090200/embedIframeJs/uiconf_id/25717641/partner_id/1660902?iframeembed=true&playerId=kaltura_player&entry_id=1_30t9q3q6">(R)</a> & Mobile Design</td>
                     <td><a href="https://github.com/CS571-F23/hw8">HW8</a></td>
                     <td>Quiz H</td>
                 </tr>
