e0ef68b77405bd50a9ca888e0907958757e3dc0b
Cole Nelson <ctnelson1997@gmail.com>
2023-11-28 17:48:08 -0600

    w13 df2

--- src/f23/components/FS23Schedule.jsx
+++ src/f23/components/FS23Schedule.jsx
@@ -88,7 +88,7 @@ export default function F23Schedule() {
                 </tr>
                 <tr>
                     <td>Nov 27<br/></td>
-                    <td>DialogFlow 2 <a target="_blank" href="https://github.com/CS571-F23/lectures/blob/main/Week13/Week13-DialogFlow2/week13-dialogflow2.pdf">(N)</a> & User Evaluation/Agency</td>
+                    <td>DialogFlow 2 <a target="_blank" href="https://github.com/CS571-F23/lectures/blob/main/Week13/Week13-DialogFlow2/week13-dialogflow2.pdf">(N)</a> <a target="_blank" href="https://cdnapisec.kaltura.com/p/1660902/sp/166090200/embedIframeJs/uiconf_id/25717641/partner_id/1660902?iframeembed=true&playerId=kaltura_player&entry_id=1_2f3gkrg5">(R)</a> & User Evaluation/Agency</td>
                     <td><a href="https://github.com/CS571-F23/hw11">HW11</a></td>
                     <td>Quiz K</td>
                 </tr>
